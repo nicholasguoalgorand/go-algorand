8c33386a6999359027877603617215d1813992ae
Nicholas Guo <nicholas.guo@algorand.com>
2021-02-03 09:19:01 -0800

    remove logging

--- cmd/pingpong/runCmd.go
+++ cmd/pingpong/runCmd.go
@@ -159,7 +159,7 @@ var runCmd = &cobra.Command{
 		if txnPerSec == 0 {
 			reportErrorf("cannot set tps to 0")
 		}
-		cfg.TxnPerSec = 800 // hardcode this value for now
+		cfg.TxnPerSec = txnPerSec
 
 		if randomFee {
 			if cfg.MinFee > cfg.MaxFee {
