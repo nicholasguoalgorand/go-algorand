187565c8a1dec6441e311b3d2d96dd9bf613dd3a
Nicholas Guo <nicholas.guo@algorand.com>
2021-02-15 22:46:15 -0800

    undo changes

--- ledger/acctupdates.go
+++ ledger/acctupdates.go
@@ -1422,7 +1422,7 @@ func (au *accountUpdates) newBlockImpl(blk bookkeeping.Block, delta ledgercore.S
 	if rnd != au.latest()+1 {
 		au.log.Panicf("accountUpdates: newBlockImpl %d too far in the future, dbRound %d, deltas %d", rnd, au.dbRound, len(au.deltas))
 	}
-	logging.Base().Infof("len deltas %v", len(au.deltas))
+	logging.Base().Infof("len deltas %v", delta.Accts.Len())
 	au.deltas = append(au.deltas, delta.Accts)
 	au.protos = append(au.protos, proto)
 	au.creatableDeltas = append(au.creatableDeltas, delta.Creatables)
