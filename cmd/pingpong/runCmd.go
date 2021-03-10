208c88aec247301c29ab2a8f53a259525e39e151
Nicholas Guo <nicholas.guo@algorand.com>
2021-03-10 13:38:23 -0800

    hardcode pingpong tps

--- cmd/pingpong/runCmd.go
+++ cmd/pingpong/runCmd.go
@@ -159,7 +159,7 @@ var runCmd = &cobra.Command{
 		if txnPerSec == 0 {
 			reportErrorf("cannot set tps to 0")
 		}
-		cfg.TxnPerSec = 800
+		cfg.TxnPerSec = 300
 
 		if randomFee {
 			if cfg.MinFee > cfg.MaxFee {
